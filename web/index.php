e6c24b0bf453966e85cf97ff90a6993400c60e57
tewcc1975 <tewcc1975@gmail.com>
2017-06-01 21:46:08 +0800

    Adding templates to the application
    Lovely template was created, it is great

--- web/index.php
+++ web/index.php
@@ -2,10 +2,12 @@
 
 require "../vendor/autoload.php";
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'templates.path' => '../templates'
+));
 
 $app->get('/', function () use ($app) {
-    echo "hello";
+    $app->render('main.php');
 });
 
 $app->run();
