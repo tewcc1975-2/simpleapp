a48bd04f09dc82a945ce1e294515385780cf5390
tewcc1975 <tewcc1975@gmail.com>
2017-06-21 21:32:27 +0800

    Change greeting words

--- web/index.php
+++ web/index.php
@@ -3,16 +3,16 @@
 require "../vendor/autoload.php";
 
 $app = new \Slim\Slim(array(
-    'templates.path' => '../templates'
+    "templates.path" => "../templates"
 ));
 
 $app->get('/', function () use ($app) {
     // just output the template
-    $app->render('main.php');
+    $app->render("main.php");
 });
 
 $app->get('/hello', function () use ($app) {
-    echo 'howdy!';
+    echo "Nice to see you";
 });
 
 $app->run();
